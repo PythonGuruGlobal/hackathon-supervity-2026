a47b2529058f4bc8d4f7a178dfcb0b967b08ded8
bhavyasreechennamsetty-cmyk <bhavyasree_chennamsetty@srmap.edu.in>
2026-01-03 15:56:35 +0530

    Delete agents directory

--- agents/symptom_agent.py
+++
@@ -1,6 +0,0 @@
-def read_symptoms(user_input: str):
-    """
-    Takes comma-separated symptoms string
-    and returns a cleaned list
-    """
-    return [s.strip().lower() for s in user_input.split(",")]
